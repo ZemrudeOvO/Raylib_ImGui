921b035864ea84617a3660268738d669c1cb9b18
Zemrude <eufonio@qq.com>
2023-05-29 18:34:17 +0800

    Initial

---
+++ Raylib_ImGui/Program.cs
@@ -0,0 +1,40 @@
+﻿using Raylib_cs;
+using ImGuiNET;
+using StoryCreator;
+
+namespace GUI
+{ 
+    public class GUI
+    { 
+        public static void Main()
+        {
+            Raylib.InitWindow(1280, 720, "Story Creator");
+            Raylib.SetTargetFPS(60);
+
+            var context = ImGui.CreateContext();
+            ImGui.SetCurrentContext(context);
+
+            var controller = new ImGuiController();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                controller.NewFrame();
+                controller.ProcessEvent();
+                ImGui.NewFrame();
+
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.DARKGRAY);
+
+                if (ImGui.Button("Click me!"))
+                {
+                    Console.WriteLine("button clicked.");
+		        }
+
+                ImGui.Render();
+                controller.Render(ImGui.GetDrawData());
+                Raylib.EndDrawing();
+	        }
+            controller.Shutdown();
+        }
+    }
+}
